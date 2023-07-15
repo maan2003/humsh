a0bfebae18726f0e5462fa5e462228d1e5d232e1
maan2003 <manmeetmann2003@gmail.com>
2023-07-16 03:13:14 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  outputs = { nixpkgs, self }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ cargo rustc rustfmt rust-analyzer ];
+    };
+  };
+}
