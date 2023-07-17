25acb671ec3b4b72bf80f298f9d7b7a3231af537
maan2003 <manmeetmann2003@gmail.com>
2023-07-17 18:20:34 +0530

    Add data for git commit

--- src/data.rs
+++ src/data.rs
@@ -61,21 +61,45 @@ fn toggle_flag(flag: &str) -> Action {
 }
 
 pub fn git_push() -> Program {
+    let push = page! {
+        "Git Push"
+
+        group "Arguments":
+            "-f" "Force with lease" => toggle_flag("--force-with-lease"),
+            "-F" "Force" => toggle_flag("--force"),
+            "-h" "Disable hooks" => toggle_flag("--no-verify"),
+            "-n" "Dry run" => toggle_flag("--dry-run"),
+
+        group "Push to":
+            "p" "origin/master" => Action::Run { exit: false },
+            "u" "upstream" => Action::Run { exit: false },
+            "p" "elsewhere" => Action::Run { exit: false },
+    };
+
+    let commit = page! {
+        "Git Commit"
+
+        group "Arguments":
+            "-a" "Stage all modified and deleted files" => toggle_flag("--all"),
+            "-e" "Allow empty commit" => toggle_flag("--allow-empty"),
+            "-v" "Show diff of changes to be committed" => toggle_flag("--verbose"),
+            "-n" "Disable hooks" => toggle_flag("--no-verify"),
+            "-R" "Claim authorship and reset author date" => toggle_flag("--reset-author"),
+            // "-A" "Override the author" => toggle_flag("--author"),
+            "-s" "Add Signed-off-by line" => toggle_flag("--signoff"),
+            // "-C" "Reuse commit message" => toggle_flag("--reuse-message"),
+
+        group "Actions":
+            "c" "Commit" => Action::Run { exit: false },
+            "e" "Extend" => Action::Run { exit: false },
+            "w" "Reword" => Action::Run { exit: false },
+            "a" "Amend" => Action::Run { exit: false },
+            "f" "Fixup" => Action::Run { exit: false },
+            "F" "Instant Fixup" => Action::Run { exit: false },
+    };
+
     Program {
-        base: CommandLine::from_iter([Arg::program("git"), Arg::subcommand("push")]),
-        start: page! {
-            "Git Push"
-
-            group "Arguments":
-                "-f" "Force with lease" => toggle_flag("--force-with-lease"),
-                "-F" "Force" => toggle_flag("--force"),
-                "-h" "Disable hooks" => toggle_flag("--no-verify"),
-                "-n" "Dry run" => toggle_flag("--dry-run"),
-
-            group "Push to":
-                "p" "origin/master" => Action::Run { exit: false },
-                "u" "upstream" => Action::Run { exit: false },
-                "p" "elsewhere" => Action::Run { exit: false },
-        },
+        base: CommandLine::from_iter([Arg::program("git"), Arg::subcommand("commit")]),
+        start: commit,
     }
 }
