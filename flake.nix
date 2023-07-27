a10ec6aa8b7351846a0be3fd2b2b30bb8db94670
maan2003 <manmeetmann2003@gmail.com>
2023-07-28 03:55:14 +0530

    add overlay

--- flake.nix
+++ flake.nix
@@ -1,10 +1,18 @@
 {
-  outputs = { nixpkgs, self }:
+  outputs = { nixpkgs, self } @ inputs:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in
   {
+    overlay = { rustPlatform, ... }: super: {
+      humsh = rustPlatform.buildRustPackage {
+        pname = "humsh";
+        version = "0.1.0";
+        src = inputs.self;
+        cargoHash = "sha256-uHwpeXFFYQ1tcMHKpI7v+Vaden7Uux36tH89t/HXChU=";
+      };
+    };
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [ cargo rustc clippy rustfmt rust-analyzer zoxide fzf fd ];
     };
