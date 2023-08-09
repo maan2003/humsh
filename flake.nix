d154267ea369c87d35899f4dc14e699ab52b55d9
maan2003 <manmeetmann2003@gmail.com>
2023-08-09 18:17:12 +0530

    update hash

--- flake.nix
+++ flake.nix
@@ -2,17 +2,19 @@
   outputs = { nixpkgs, self } @ inputs:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
     overlay = self: super: {
       humsh = self.rustPlatform.buildRustPackage {
         pname = "humsh";
         version = "0.1.0";
         src = inputs.self;
-        cargoHash = "sha256-uHwpeXFFYQ1tcMHKpI7v+Vaden7Uux36tH89t/HXChU=";
+        cargoHash = "sha256-yKISYvlKH+Gzrp6jjAK9nX6xQWXmT9m510LSsn1KkfY=";
       };
     };
+    pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+  in
+  {
+    inherit overlay;
+    packages.${system}.default = pkgs.humsh;
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [ cargo rustc clippy rustfmt rust-analyzer zoxide fzf fd ];
     };
